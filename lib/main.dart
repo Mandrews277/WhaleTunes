e1301cf8efe5a7c6853e87bffc5d767016db050a
stoutlegs <mandrews277@gmail.com>
2023-01-23 12:55:17 -0800

    Basic AWS amplify skeleton code setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'amplifyconfiguration.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WhaleTunes());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WhaleTunes extends StatefulWidget {
+  const WhaleTunes({super.key});
+
+  @override
+  State<WhaleTunes> createState() => _WhaleTunesState();
+}
+
+class _WhaleTunesState extends State<WhaleTunes> {
+  @override
+  initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    // Add any Amplify plugins you want to use
+    final authPlugin = AmplifyAuthCognito();
+    await Amplify.addPlugin(authPlugin);
+
+    // You can use addPlugins if you are going to be adding multiple plugins
+    // await Amplify.addPlugins([authPlugin, analyticsPlugin]);
+
+    // Once Plugins are added, configure Amplify
+    // Note: Amplify can only be configured once.
+    try {
+      await Amplify.configure(amplifyconfig);
+    } on AmplifyAlreadyConfiguredException {
+      safePrint(
+          "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
+    }
+  }
 
   // This widget is the root of your application.
   @override
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:whaletunes/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const WhaleTunes());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
